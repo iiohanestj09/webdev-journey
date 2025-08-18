075246c49bf0b3f1c2003f1bb8b7433363d5a062
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-18 20:45:04 +0700

    Update 3,a - Rapiin kode

--- 3_JavaScript-web/a_dasar_DOM/2_seleksi_elemen/script.js
+++ 3_JavaScript-web/a_dasar_DOM/2_seleksi_elemen/script.js
@@ -1,8 +1,7 @@
 /* 1. Berdasarkan ID
 * document.getElementById("namaId");
   - Mengembalikkan 1 elemen dengan id yg cocok
-  - Cepat, tapi hanya utk 1 elemen karena id harus unik di HTML.
-*/
+  - Cepat, tapi hanya utk 1 elemen karena id harus unik di HTML. */
 const judul = document.getElementById("judul");
 console.log(judul);       
 
@@ -10,8 +9,7 @@ console.log(judul);
 /* 2. Berdasarkan Nama Tag
 * document.getElementsByTagName("namaTag");
   - Mengembalikkan HTMLCollection (Mirip array tapi bukan array asli)
-  - Bisa diakses dengan indeks [0], [1], ...  
-*/
+  - Bisa diakses dengan indeks [0], [1], ...   */
 const paragraf = document.getElementsByTagName("p");
 console.log(paragraf[0]);
 console.log(paragraf[0].textContent);
@@ -21,8 +19,7 @@ console.log(paragraf[1]);
 /* 3. Berdasarkan Nama Class
 * document.getElementsByClassName("namaClass");
   - Mengembalikkan HTMLCollection.
-  - Bisa ada banyak elemen dengan class yg sama
-*/
+  - Bisa ada banyak elemen dengan class yg sama */
 const ket = document.getElementsByClassName("keterangan");
 console.log(ket[0]);
 
@@ -30,8 +27,7 @@ console.log(ket[0]);
 /* 4. Query Selector (CSS Selector)
 * document.querySelector("selector");
   - Menggunakan CSS Selector seperti di CSS (#id, .class, tag, kombinasi).
-  - Mengembalikkan ELEMEN PERTAMA yg cocok.
-*/ 
+  - Mengembalikkan ELEMEN PERTAMA yg cocok. */ 
 console.log(document.querySelector("#judul"));
 console.log(document.querySelector("p"));     // Sama
 console.log(document.querySelector("p"));     // Sama
@@ -42,8 +38,7 @@ console.log(document.querySelector("p.coba"));
 /* 5. Query Selector All
 * document.querySelectorAll("selector");
   - Mengembalikkan NodeList (Mirip array, bisa di-loop dgn forEach)
-  - Cocok utk memilih SEMUA elemen yg cocok dgn selector
-*/
+  - Cocok utk memilih SEMUA elemen yg cocok dgn selector */
 const semuaP = document.querySelectorAll("p");
 semuaP.forEach((el, index) => {
   console.log(`${index} -> ${el.textContent}`);
--- 3_JavaScript-web/a_dasar_DOM/3_manipulasi_konten/script.js
+++ 3_JavaScript-web/a_dasar_DOM/3_manipulasi_konten/script.js
@@ -4,8 +4,7 @@
 => Fungsi: Mengambil / Mengubah teks MENTAH dalam elemen.
   - Selalu mengembalikkan semua teks (termasuk teks yg tersembunyi dgn CSS)
   - Tidak memproses HTML tag, hanya menampilkan teksnya (isinya)
-  - Semua tag HTML akan dianggap sebagai teks biasa di dalamnya, maka tidak bisa menjalankan Script atau HTML
-*/
+  - Semua tag HTML akan dianggap sebagai teks biasa di dalamnya, maka tidak bisa menjalankan Script atau HTML */
 const p1 = document.getElementById("contoh");
 console.log(p1.textContent);
 // const sembunyi = document.getElementsByTagName("span");
@@ -20,8 +19,7 @@ console.log(p1.textContent);
 => Fungsi: Mengambil / Mengubah konten HTML mentah dalam elemen.
   - Mengembalikkan string HTML (termasuk tag)
   - Bisa digunakan utk menyisipkan HTML baru
-  - berbahaya kalau isi berasal dari user input. (Rentan XSS(Cross-Stire Scripting))
-*/
+  - berbahaya kalau isi berasal dari user input. (Rentan XSS(Cross-Stire Scripting)) */
 const p2 = document.getElementById("contoh2");
 console.log(p2.innerHTML);
 
@@ -33,8 +31,7 @@ console.log(p2.innerHTML);
 => Fungsi: Mengambil / Mengubah teks yg terlihat di layar (rendered text).
   - Tidak mengambil teks yg tersembunyi (display:none atau visibility:hidden)
   - Memperhatikan style CSS (misalnya line-break)
-  - Lebih lambat dari .textContent karena harus menghitung gaya CSS
-*/
+  - Lebih lambat dari .textContent karena harus menghitung gaya CSS */
 const p3 = document.getElementById("contoh3");
 console.log(p3.innerText);
 
